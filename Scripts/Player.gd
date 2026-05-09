ef2fc66d3af309d92545f888e3a4ea6e88c6e3c4
Cassiana945 <ametista272021@outlook.com>
2026-05-09 22:14:53 +0000

    plataformas que caem

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -25,7 +25,10 @@ func _physics_process(delta: float) -> void:
 	
 	_set_animation()
 	
-	
+	for platforms in get_slide_count():
+		var collision = get_slide_collision(platforms)
+		if collision.collider.has_method("collide_with"):
+			collision.collider.collide_with(collision, self)
 		
 func _get_input():		
 	velocity.x = 0
